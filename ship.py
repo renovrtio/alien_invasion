979562cea8b33f5bfd144c2c67f54b77c62646b9
renovrtio <qin_anqi@foxmail.com>
2018-02-17 13:37:47 +0800

    test

---
+++ ship.py
@@ -0,0 +1,47 @@
+import pygame
+import sys
+
+class Ship():
+
+    def __init__(self, ai_settings, screen):
+        """初始化飞船并设置其初始位置"""
+        self.screen = screen
+        self.ai_settings = ai_settings
+        # 加载飞船图像冰洁去其外接矩形
+
+        self.image = pygame.image.load(sys.path[0] + r'\images\ship.bmp')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
+        # 将每艘新飞船放在屏幕的底部中央
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+        # 在飞船的属性center中存储小数值
+        self.center = float(self.rect.centerx)
+        self.hight = float(self.rect.bottom)
+
+        # 移动标志
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
+    
+    def blitme(self):
+        """在指定位置绘制飞船"""
+        self.screen.blit(self.image, self.rect)
+    
+    def update(self):
+        """根据移动标志调整飞船的位置"""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+        elif self.moving_left and self.rect.left > 0:
+            self.center -= self.ai_settings.ship_speed_factor
+        elif self.moving_up and self.rect.top > 0:
+            self.hight -= self.ai_settings.ship_speed_factor
+        elif self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.hight += self.ai_settings.ship_speed_factor
+        
+        # 根据self.center更新rect对象
+        self.rect.centerx = self.center
+        self.rect.bottom = self.hight
